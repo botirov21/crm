fb81baa580309e94da65f36f82975357ecf8e6da
Rasheed <teambozorov@gmail.com>
2024-06-19 19:41:04 +0900

    project added

---
+++ src/components/home/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../others/navbar/navbar'
+import Footer from '../others/footer/footer'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
