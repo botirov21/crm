d3d4f7dac44bbbddfd1cad1c0455f40dbad4b822
uniquebr <zikitri2004@gmail.com>
2024-06-22 15:08:51 +0900

    added mock data

---
+++ src/components/mock/teachersData.js
@@ -0,0 +1,64 @@
+export const card = {
+    TacherList: [
+      {
+        id: 1,
+        teacher: {
+          name: "Putin",
+          number: "998949596",
+          group: "Elemtry",
+          percent: "40%",
+         
+        },
+      },
+      {
+        id: 2,
+        teacher: {
+          name: "Makron",
+          number: "998949596",
+          group: "Elemtry",
+          percent: "40%",
+         
+        },
+      },
+      {
+        id: 3,
+       teacher: {
+          name: "Kim",
+          number: "998949596",
+          group: "Elemtry",
+          percent: "40%",
+         
+        },
+      },
+      {
+        id: 4,
+         teacher: {
+          name: "Biden",
+          number: "998949596",
+          group: "Elemtry",
+          percent: "40%",
+         
+        },
+      },
+      {
+        id: 5,
+     teacher: {
+          name: "nikola",
+          number: "998949596",
+          group: "Elemtry",
+          percent: "40%",
+         
+        },
+      },
+      {
+        id: 6,
+       teacher: {
+          name: "Merkel",
+          number: "998949596",
+          group: "Elemtry",
+          percent: "40%",
+         
+        },
+      },
+    ],
+  };
