461593c9c7c4e09b0fb4baf3d5e71f00ee3a712c
Rasheed <teambozorov@gmail.com>
2024-06-29 19:35:50 +0900

    leads almost finished

--- src/components/pages/leads/hMenu.jsx
+++ src/components/pages/leads/hMenu.jsx
@@ -1,11 +1,127 @@
-import React from 'react';
+import React from "react";
+import {
+  FromWhereBtn,
+  FromWhereCont,
+  HMenuContainer,
+  SectionsCont,
+} from "./leadsStyle";
+import Box from "@mui/material/Box";
+import AddIcon from "@mui/icons-material/Add";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import {
+  AddTeacherBtn,
+  ModalComponent,
+  ModalInput,
+  ModalLabel,
+} from "../teachers/teachersStyle";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const HMenu = () => {
+  const [whereOpen, setWhereOpen] = React.useState(false);
+  const [sectionOpen, setSectionOpen] = React.useState(false);
+  // from where 
+  const handleWhereOpen = () => setWhereOpen(true);
+  const handleClose = () => setWhereOpen(false);
+
+  //sections
+   const handleSectionOpen = () => setSectionOpen(true);
+  const handleSectionClose = () => setSectionOpen(false); 
   return (
-    <div style={{ height: 400, width: '100%', background: "#fff" }}>
-    h menu
-    </div>
+    <HMenuContainer>
+      <FromWhereCont>
+        <h1>From Where</h1>
+        <FromWhereBtn onClick={handleWhereOpen}>
+          <AddIcon />
+        </FromWhereBtn>
+        <Modal
+          open={whereOpen}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <ModalComponent>
+              <Typography
+                id="modal-modal-title"
+                variant="h6"
+                component="h2"
+                sx={{
+                  color: "#2C2669",
+                  fontSize: "24px",
+                  fontWeight: "700",
+                }}
+              >
+                Add from where
+              </Typography>
+              <div>
+                <ModalLabel>From where name</ModalLabel>
+                <ModalInput placeholder="Enter name" type="text" />
+              </div>
+              <div
+                style={{
+                  paddingTop: "20px",
+                }}
+              >
+                <AddTeacherBtn>Create from where</AddTeacherBtn>
+              </div>
+            </ModalComponent>
+          </Box>
+        </Modal>
+      </FromWhereCont>
+      <SectionsCont>
+        <h1>Sections</h1>
+        <FromWhereBtn onClick={handleSectionOpen}>
+          <AddIcon />
+        </FromWhereBtn>
+         <Modal
+          open={sectionOpen}
+          onClose={handleSectionClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <ModalComponent>
+              <Typography
+                id="modal-modal-title"
+                variant="h6"
+                component="h2"
+                sx={{
+                  color: "#2C2669",
+                  fontSize: "24px",
+                  fontWeight: "700",
+                }}
+              >
+                Add section
+              </Typography>
+              <div>
+                <ModalLabel>Section name</ModalLabel>
+                <ModalInput placeholder="Enter section name" type="text" />
+              </div>
+              <div
+                style={{
+                  paddingTop: "20px",
+                }}
+              >
+                <AddTeacherBtn>Create section</AddTeacherBtn>
+              </div>
+            </ModalComponent>
+          </Box>
+        </Modal>
+      </SectionsCont>
+    </HMenuContainer>
   );
-}
+};
 
 export default HMenu;
--- src/components/pages/leads/leadsStyle.js
+++ src/components/pages/leads/leadsStyle.js
@@ -1,29 +1,29 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
- export const LeadsContainer=styled.div`
- display: flex;
- flex-direction: column;
- justify-content: center;
- align-items: center;
- width: 100%;
- height: 100vh;
- background: var(--100, #EFEEF8);
- `
- export const LeadsNav=styled.article`
- display: flex;
- flex-direction: row;
- justify-content: safe;
- align-items: center;
- gap: 20px;
- `
- export const LeadsMenu=styled.div`
-width: 60%;
-  height:50px;
+export const LeadsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background: var(--100, #efeef8);
+`;
+export const LeadsNav = styled.article`
+  display: flex;
+  flex-direction: row;
+  justify-content: safe;
+  align-items: center;
+  gap: 20px;
+`;
+export const LeadsMenu = styled.div`
+  width: 60%;
+  height: 50px;
   display: flex;
   justify-content: center;
   align-items: center;
-  .menu-handle{
-     display: flex;
+  .menu-handle {
+    display: flex;
     flex-direction: column;
     width: 107px;
     height: 40px;
@@ -38,13 +38,78 @@ width: 60%;
     font-weight: 500;
     line-height: 16px;
   }
- `
- export const MenuDisplay=styled.div`
- display: flex;
- justify-content: start;
- margin-top: 60px;
- align-items: center;
- `
+`;
+export const MenuDisplay = styled.div`
+  display: flex;
+  justify-content: start;
+  margin-top: 60px;
+  align-items: center;
+`;
 
- //H Menu style 
- 
+//H Menu style
+
+export const HMenuContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: start;
+  height: 80vh;
+  width: 100%;
+
+  background-color: #efeef8;
+  gap: 30px;
+`;
+
+export const FromWhereCont = styled.article`
+  width: 387px;
+  height: 662px;
+  flex-shrink: 0;
+  border-radius: 16px;
+  background: var(--Color-7, #fff);
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  padding: 20px;
+  h1 {
+    color: var(--600, #2c2669);
+    font-family: "Public Sans";
+    font-size: clamp(22px, 2vw+1rem, 30px);
+    font-style: normal;
+    font-weight: 700;
+    line-height: 30px;
+  }
+`;
+export const FromWhereBtn = styled.button`
+  display: flex;
+  width: 347px;
+  height: 44px;
+  padding: 10px 40px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  flex-shrink: 0;
+  border-radius: 6px;
+  border: 1px solid var(--600, #2c2669);
+  cursor: pointer;
+  background: #fff;
+`;
+export const SectionsCont = styled.article`
+  width: 387px;
+  height: 916px;
+  flex-shrink: 0;
+  border-radius: 16px;
+  background: var(--Color-7, #fff);
+    display: flex;
+  flex-direction: column;
+  align-items: start;
+  padding: 20px;
+    h1 {
+    color: var(--600, #2c2669);
+    font-family: "Public Sans";
+    font-size: clamp(22px, 2vw+1rem, 30px);
+    font-style: normal;
+    font-weight: 700;
+    line-height: 30px;
+  }
+`;
