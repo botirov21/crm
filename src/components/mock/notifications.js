7ef273005b31e05d9d575956587115548bab6c4b
uniquebr <zikitri2004@gmail.com>
2024-07-11 23:53:09 +0900

    notification finished

--- src/components/mock/notifications.js
+++ src/components/mock/notifications.js
@@ -5,7 +5,8 @@ export const mockNotification= {
             notification: {
                 title: 'Test uchun yaratilgan blog',
                 text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
-                view: '165'
+                view: '165',
+                date: '05.18.2024 - 12:24'
             }
         },
         {
@@ -13,31 +14,32 @@ export const mockNotification= {
             notification: {
                 title: 'Test uchun yaratilgan blog',
                 text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
-                view: '165'
-            }
+                view: '165',
+                date: '05.18.2024 - 12:24'            }
         },
         {
             id: 3,
             notification: {
                 title: 'Test uchun yaratilgan blog va tekstlari ko’p, ikki qator joylashadi.',
                 text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
-                view: '16k'
-            }
+                view: '165',
+                date: '05.18.2024 - 12:24'            }
         },
         {
             id: 4,
             notification: {
                 title: 'Test uchun yaratilgan blog',
                 text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
-                view: '300'
-            }
+                view: '165',
+                date: '05.18.2024 - 12:24'            }
         },
         {
             id: 5,
             notification: {
                 title: 'Test uchun yaratilgan blog',
                 text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
-                view: '921'
+                view: '165',
+                date: '05.18.2024 - 12:24'
             }
         },
     ]
---
+++ src/components/pages/notifications/notificationModal.jsx
@@ -0,0 +1,68 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import AddIcon from "@mui/icons-material/Add";
+import Modal from "@mui/material/Modal";
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import {
+  AddButton,
+  CreateBtn,
+  InputWrapper,
+  ModalComponent,
+  ModalInput,
+  ModalLabel,
+  ModalTextarea,
+} from "./notificationsStyle";
+import { useState } from "react";
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function NotificationModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [value, setValue] = useState('');
+
+  return (
+    <div>
+      <AddButton onClick={handleOpen}>
+        {" "}
+        <AddIcon />
+        Add new category
+      </AddButton>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <ModalComponent>
+            <h1>Create new blog</h1>
+            <InputWrapper>
+              <ModalLabel>Title</ModalLabel>
+              <ModalInput placeholder="Enter name" type="text" />
+            </InputWrapper>
+            <InputWrapper>
+              <ModalLabel>Content</ModalLabel>
+
+              <ReactQuill placeholder="Enter text" className="modalTextarea" theme="snow" value={value} onChange={setValue} />
+            </InputWrapper>
+            <InputWrapper>
+              <CreateBtn>Create blog </CreateBtn>
+            </InputWrapper>
+          </ModalComponent>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
